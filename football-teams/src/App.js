3ebee7b81510b92645c634f5bd4b389700054731
MoriyaEster <moriyaeo100@gmail.com>
2025-01-05 22:35:25 +0200

    start with react

---
+++ football-teams/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="bg-green-200 text-center py-10">
+      <h1 className="text-3xl font-bold">Tailwind CSS is Working!</h1>
+    </div>
+  );
+}
