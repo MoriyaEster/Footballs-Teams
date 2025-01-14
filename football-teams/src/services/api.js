c7a8cf9dfd6188a52c4372cc0a1869a3171daf84
MoriyaEster <moriyaeo100@gmail.com>
2025-01-14 17:50:14 +0200

    FINISH

--- football-teams/src/services/api.js
+++ football-teams/src/services/api.js
@@ -11,4 +11,4 @@ const apiClient = axios.create({
 export default apiClient;
 
 // API key - 44bc882e7ec1d769ec7cf4eeb170a762
-//ORI key - 528d45c9b4d41c33b7b813fbaeda7d25
+// API2 key - 528d45c9b4d41c33b7b813fbaeda7d25
